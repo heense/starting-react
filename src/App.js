3522ebfc97f68453ebea9851c5c0d2a5a2fcca24
HRM <hrm@novicell.dk>
2023-07-10 21:40:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
+
+import PropTypes from "prop-types";
+
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr>  
+    <td>
+      {pokemon.name.english}
+    </td>
+    <td>
+        {pokemon.type.join(', ')}
+    </td>
+    <td>
+      <Button variant="contained" color="primary" onClick={() => onSelect(pokemon)} >Select!</Button>
+    </td>
+  </tr>  
+);
+
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      <tbody>
+        {Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+)
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string.isRequired
+    }),
+    type: PropTypes.arrayOf(PropTypes.string.isRequired)
+  }),
+  onSelect: PropTypes.func.isRequired
+};
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
 
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState([]);
+  const [selectedItem, selectedItemSet] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/starting-react/pokemon.json')
+      .then(resp => resp.json())
+      .then(data => pokemonSet(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Pokemon search</Title>
+
+      <TwoColumnLayout>
+        <div>
+          <Input       
+            value={filter}
+            onChange={(e) => filterSet(e.target.value)}
+          />
+          <table width="100%">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon.filter((pokemon) => pokemon.name.english.toLocaleLowerCase().includes(filter.toLocaleLowerCase())).slice(0, 20).map(pokemon => (  
+                <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => selectedItemSet(pokemon)} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import CssBaseLine from '@mui/material/Button';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseLine />
     <App />
   </React.StrictMode>
 );
